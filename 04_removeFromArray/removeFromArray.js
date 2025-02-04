feb42344dbd4cd2dc6037fbb1630badceb8d9a84
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 00:37:57 +0800

    complete excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    for(const number of numbers){
+        let index = array.indexOf(number)
+        while (index > -1){
+            array.splice(index,1)
+            index = array.indexOf(number)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
