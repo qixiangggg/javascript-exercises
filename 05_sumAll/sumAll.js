91ad151df9b67f4958c30bf2dfe132f9b8c7e045
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 02:24:16 +0800

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    console.log(typeof start)
+    if (start < 0 || end <0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR"
+    }
+    if (start > end){
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    let ret = 0;
+    for(let i=start; i<=end;i++){
+        ret +=i;
+        console.log(i)
+    }
+    return ret;
+    
 };
-
+console.log(sumAll(2.5, 4))
 // Do not edit below this line
 module.exports = sumAll;
