8d87edf5ce792af3258daea32c9ccf4d86e2ab31
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-04 23:17:53 +0800

    complete exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    new_string = string.split('');
+    let ret = "";
+    for(let i=string.length-1; i>=0;i--){
+        ret += new_string[i];
+    }
+    return ret;
 };
 
 // Do not edit below this line
