24677eadafa81c1311ad7836f0f689e419d6270d
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 18:06:12 +0800

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const title = [];
+    for(let i = 0; i<books.length; i++){
+       title.push(books[i].title) 
+    }
+    return title;
 };
 
 // Do not edit below this line
