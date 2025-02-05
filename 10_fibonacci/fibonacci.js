22a29f5eb14d4dcbe2bd3678377ee060b6e5c73f
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 17:19:15 +0800

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let ret;
+    if (number < 0){
+        return "OOPS";
+    }
+    if (number == 0){
+        ret = 0;
+    }
+    else if (number == 1){
+        ret = 1;
+    }
+    else if (number >= 2){
+        num1 = 0
+        num2 = 1
+        for (let i = 2; i<=number;i++){
+            ret = num1 + num2;
+            temp = num2;
+            num2 = ret;
+            num1 = temp;
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
