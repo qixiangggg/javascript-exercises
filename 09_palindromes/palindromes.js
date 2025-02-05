fdff1222b3447327898d2a8b6050f9a337ecfc38
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 17:01:17 +0800

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    validWord = "abcdefghijklmnopqrstuvwxyz0123456789";
+    cleanedWord = string.toLowerCase().split("").filter((letter)=>validWord.includes(letter)).join("");
+    reverseWord = cleanedWord.split("").reverse().join("");
+    return reverseWord === cleanedWord;
 };
 
 // Do not edit below this line
