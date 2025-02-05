35a6dbb61c53020a215eff5a305661c582532590
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 15:54:00 +0800

    complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  for(const number of numbers){
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let multiply = 1;
+  numbers.forEach(num=>multiply *= num);
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0){
+    return 1;
+  }
+  else{
+    let ret = num;
+    num--;
+    for(; num>=1; num--){
+      ret*=num;
+    }
+    return ret;
+  }
 };
 
 // Do not edit below this line
