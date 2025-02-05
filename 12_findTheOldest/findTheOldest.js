27cc2c3ceba9054d98937f4d8e163ea6c9081d58
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-02-05 22:52:10 +0800

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findAge = function(birth,death){
+    if (!death){
+        const currentDate = new Date();
+        return currentDate.getFullYear() - birth;
+    }
+    return death - birth;
+}
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson,currentPerson)=>{
+        let currentPersonAge = findAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        let oldestPersonAge = findAge(oldestPerson.yearOfBirth,oldestPerson.yearOfDeath);
+        return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+    })
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+// find the age of every people
